ce5865bf7b6952eaad04491455e6a8c630f32dc6
mikkeg05 <mikkelgunge@hotmail.com>
2021-10-08 11:23:14 +0200

    cors

---
+++ src/main/java/utils/CorsFilter.java
@@ -0,0 +1,21 @@
+package utils;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@PreMatching
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestCtx, ContainerResponseContext res )
+            throws IOException {
+        res.getHeaders().add("Access-Control-Allow-Origin", "*" );
+        res.getHeaders().add("Access-Control-Allow-Credentials", "true" );
+        res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+        res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization,x-access-token");
+    }
+}
